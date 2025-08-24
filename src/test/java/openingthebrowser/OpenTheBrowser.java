fed0adea3e6a2c5342e76b742c570224025a7b66
JAS1007 <imjasvathsa@gmail.com>
2025-08-24 10:21:27 +0530

    I have created an automation script using java and testng to open the browser and navigate to a website

---
+++ src/test/java/openingthebrowser/OpenTheBrowser.java
@@ -0,0 +1,29 @@
+package openingthebrowser;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class OpenTheBrowser {
+	WebDriver driver;
+	
+	@BeforeTest
+	public void openBrowser() {
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.get("https://www.youtube.com/");
+	}
+	
+	@Test
+	public void searchSomtehing() {
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.findElement(By.xpath("//div[contains(@class,'ytSearchboxComponentInput')]")).click();
+		driver.findElement(By.xpath("//div[contains(@class,'ytSearchboxComponentInput')]")).sendKeys("ramayana trailer");
+		driver.findElement(By.className("ytSearchboxComponentSearchButton")).click();
+	}
+}
